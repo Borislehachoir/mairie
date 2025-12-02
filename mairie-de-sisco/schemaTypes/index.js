e5f107b49b7cb42170674c82ad7bf4b89774ef28
Borislehachoir <borisrocc@gmail.com>
2025-12-02 20:44:49 +0100

    je dégage sanity on se débrouillera autrement

--- mairie-de-sisco/schemaTypes/index.js
+++
@@ -1,6 +0,0 @@
-import blockContent from '../../mairie-de-sisco/schemaTypes/blockContent'
-import category from './category'
-import post from './article'
-import author from '../../mairie-de-sisco/schemaTypes/author'
-
-export const schemaTypes = [post, author, category, blockContent]
